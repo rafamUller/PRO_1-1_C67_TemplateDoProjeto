db120d682f84f1c2d823a7fb8aaf2c630d00bad5
rafamUller <115031919+rafamUller@users.noreply.github.com>
2023-08-14 17:48:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyAlRwLP56dAOc3-cc61doUc-QuBwhLSWdE",
+  authDomain: "aula-dever.firebaseapp.com",
+  projectId: "aula-dever",
+  storageBucket: "aula-dever.appspot.com",
+  messagingSenderId: "428966743169",
+  appId: "1:428966743169:web:20771b4731267e3d3bd180"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
